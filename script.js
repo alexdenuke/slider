6bf0915a1a407f30d60ffbbb19b8c9f6a2b97ccc
alexdenuke <alexdenuke@gmail.com>
2022-05-20 04:25:26 +0300

    fork

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let slides = document.querySelectorAll('.img');
-let slider = [];
-let length = 0;
+// let slides = document.querySelectorAll('.img');
+// let slider = [];
+// let length = 0;
 
 // for (let i = 0; i < slides.length; i++) {
 //     slider[i] = slides[i].src;
@@ -20,46 +20,46 @@ let length = 0;
 //     }
 // }
 
-let step = 0;
+// let step = 0;
 
 
-let btnLeft = document.querySelector('.button-left');
-let btnRight = document.querySelector('.button-right');
-let step2 = 0;
-let position = 0;
-let active = 0;
+// let btnLeft = document.querySelector('.button-left');
+// let btnRight = document.querySelector('.button-right');
+// let step2 = 0;
+// let position = 0;
+// let active = 0;
 
-function left() {
-    let line = document.querySelector('.line');
-    let div = line.cloneNode(true);
-    console.log(div);
-    active--;
-    if (active === -1) {
-        alert('меньше');
-    }
-    else {
-        line.style.right = position - 300 + 'px';
-        position -= 300;
-    }
-}
-function right() {
-    let line = document.querySelector('.line');
-    // let last = document.querySelector('.last');
-    let div = line.cloneNode(true);
-    div.style.right = -1515 + "px";
-    console.log(div);
-    active++;
+// function left() {
+//     let line = document.querySelector('.line');
+//     let div = line.cloneNode(true);
+//     console.log(div);
+//     active--;
+//     if (active === -1) {
+//         alert('меньше');
+//     }
+//     else {
+//         line.style.right = position - 300 + 'px';
+//         position -= 300;
+//     }
+// }
+// function right() {
+//     let line = document.querySelector('.line');
+//     // let last = document.querySelector('.last');
+//     let div = line.cloneNode(true);
+//     div.style.right = -1515 + "px";
+//     console.log(div);
+//     active++;
 
-    if (active == slides.length) {
-        line.append(div);
-        active = -1;
-    }
-    else {
-        line.style.right = position + 300 + 'px';
-        position += 300;
-    }
+//     if (active == slides.length) {
+//         line.append(div);
+//         active = -1;
+//     }
+//     else {
+//         line.style.right = position + 300 + 'px';
+//         position += 300;
+//     }
 
-}
+// }
 
-btnLeft.onclick = left;
-btnRight.onclick = right;
+// btnLeft.onclick = left;
+// btnRight.onclick = right;
